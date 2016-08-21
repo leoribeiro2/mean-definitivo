3a4951656a96850463501c1599cfa7f41e7dc3d4
Leonardo Ribeiro WEB Developer <contato@leoribeiroweb.com.br>
2016-08-21 19:23:09 -0300

    capitulo 3 finalizado

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-
-var homepageController = function (req, res) {
-  res.render('index', {title: 'Express'});
-};
+var ctrlMain = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', homepageController);
+router.get('/', ctrlMain.index);
 
 module.exports = router;
