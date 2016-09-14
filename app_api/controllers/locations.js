c1a65165882239cc95e379b72c5fda19a5c23de4
Leonardo Ribeiro WEB Developer <contato@leoribeiroweb.com.br>
2016-09-13 23:01:40 -0300

    'colocando api'

---
+++ app_api/controllers/locations.js
@@ -0,0 +1,6 @@
+module.exports.locationsCreate = function (req, res) { };
+module.exports.locationsListByDistance = function (req, res) { };
+module.exports.locationsReadOne = function (req, res) { };
+module.exports.locationsUpdateOne = function (req, res) { };
+module.exports.locationDeleteOne = function (req, res) { };
+
