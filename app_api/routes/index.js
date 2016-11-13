856f06ba2dbe7c182873f24320ad711cb0d2c431
Leonardo Ribeiro WEB Developer <contato@leoribeiroweb.com.br>
2016-11-13 13:18:04 -0200

    quase terminando a api

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,20 +1,18 @@
 var express = require('express');
 var router = express.Router();
-
 var ctrlLocations = require('../controllers/locations');
 var ctrlReviews = require('../controllers/reviews');
 
-// estabelecimentos(locations)
 router.get('/locations', ctrlLocations.locationsListByDistance);
 router.post('/locations', ctrlLocations.locationsCreate);
 router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
 router.put('/locations/:locationid', ctrlLocations.locationsUpdateOne);
-router.delete('/locations/:locationid', ctrlLocations.locationDeleteOne);
+router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
 
-// resenhas (reviews)
+// reviews
 router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
 router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
 router.put('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
-router.delete('locartions:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
+router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
 
 module.exports = router;
